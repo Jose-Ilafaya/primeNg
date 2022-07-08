f3075b1d8ea39f670087319d78a6b67c9b928a01
Jose-Ilafaya <ilafayajose@gmail.com>
2022-07-08 11:07:15 -0300

    viernes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import {MenuItem} from 'primeng/api';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation:ViewEncapsulation.None
 })
 export class AppComponent {
+  mostrar(){
+    this.display=true;
+  }
   title = 'primeNg';
+  display:boolean=false;
   items: MenuItem[]=[];
+  cachorro:string[]=[
+    "https://cdn.shopify.com/s/files/1/0248/4692/5912/articles/1519305023-0-0_4deff7f8-587a-4500-9aef-ca5063e27da1_1024x1024.jpg?v=1567518338",
+    "http://cdn.shopify.com/s/files/1/0560/3241/files/8165539512_cd5c25de26_h_grande.jpg?2227402961487937639",
+    "https://www.erapetfood.com/wp-content/uploads/2021/10/cachorro-corriendo_2.jpg",
+    "https://www.erapetfood.com/wp-content/uploads/2021/10/cachorro-corriendo_2.jpg"
+  ];
+	responsiveOptions:any[]=[];
   constructor(){
-    
+    this.responsiveOptions = [
+      {
+          breakpoint: '1024px',
+          numVisible: 3,
+          numScroll: 3
+      },
+      {
+          breakpoint: '768px',
+          numVisible: 2,
+          numScroll: 2
+      },
+      {
+          breakpoint: '560px',
+          numVisible: 1,
+          numScroll: 1
+      }
+  ];
   }
     ngOnInit() {
         this.items = [
             {
-                label: 'File',
-                items: [{
-                        label: 'New', 
-                        icon: 'pi pi-fw pi-plus',
-                        items: [
-                            {label: 'Project'},
-                            {label: 'Other'},
-                        ]
-                    },
-                    {label: 'Open'},
-                    {label: 'Quit'}
-                ]
+                label: 'Home',
+                icon:'pi pi-home'
             },
             {
                 label: 'Edit',
@@ -36,5 +54,6 @@ export class AppComponent {
                 ]
             }
         ];
+        
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MenubarModule} from 'primeng/menubar';
+import {CarouselModule} from 'primeng/carousel';
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+import {DialogModule} from 'primeng/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    MenubarModule
+    MenubarModule,
+    CarouselModule,
+    CardModule,
+    ButtonModule,
+    DialogModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
